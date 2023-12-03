9ffc8537d1dce4f5ddf2d2fd2670eb83778a6d99
ClawhammerLobotomy <dansleeman@gmail.com>
2023-12-03 16:05:56 -0500

    Test input file for day 2

--- day_2.py
+++ day_2.py
@@ -29,16 +29,10 @@ class Rounds:
         self.__dict__.update(**r_dic)
 
 input_file = 'd2_input.txt'
+# input_file = 'd2_test_input.txt'
 
 with open(input_file, 'r') as f:
     lines = f.readlines()
-    # lines = [
-    #     'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-    #     'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-    #     'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-    #     'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-    #     'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
-    # ]
     game_list = [Game(l) for l in lines]
     id_sum = sum([x.id for x in game_list if x.invalid == False])
     power_sum = sum([x.power for x in game_list])
