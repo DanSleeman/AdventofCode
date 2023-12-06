71bc8dd353cd7a73eb5204c4ea022002df73d79c
Dan Sleeman <sleemand@shapecorp.com>
2023-12-06 15:58:28 -0500

    day5

--- day_5.py
+++ day_5.py
@@ -1,7 +1,7 @@
 from collections import namedtuple, defaultdict
 from itertools import chain
 INPUT_FILE = 'd5_input.txt'
-INPUT_FILE = 'd5_test_input.txt'
+# INPUT_FILE = 'd5_test_input.txt'
 
 with open(INPUT_FILE, 'r') as f:
     LINE_LIST = f.read()
@@ -9,6 +9,8 @@ with open(INPUT_FILE, 'r') as f:
 """
 Each mapping needs a dictionary of from:to pairs in the range
 Starting values = list(range(x[1],x[1]+x[2]))
+Mapping values = list(range(int(x[0]),int(x[0])+int(x[2]))
+Need a default dictionary with matching key:value pairs for the range of the lowest and highest seeds
 Seed={
 98:50, (50 98 2)
 99:51, 
@@ -17,25 +19,48 @@ Seed={
 97:99
 }
 """
-class Mapping():
-    def __init__(self,first,rest):
-        self.map_from, to, self.map_to = first.split()[0].split('-')
-        self.map_ranges = rest
 
-Mapping = namedtuple('Mapping',['map_from','map_to','source_start','destination_start','range_length'])
+
+# Mapping = namedtuple('Mapping',['map_from','map_to','source_start','destination_start','range_length'])
 parts = LINE_LIST.split('\n\n')
-maps = defaultdict(int)
+
+almanac = defaultdict(dict)
+almanac_key = defaultdict(str)
 # maps = []
 for l in parts:
     first, rest = l.split(':')
     if 'seeds' in first:
         seed_list = rest.split()
+        dict_range_start = int(min(seed_list))
+        dict_range_end = int(max(seed_list))
     else:
+        maps = defaultdict(int)
         map_from, to, map_to = first.split()[0].split('-')
+        almanac_key[map_from] = map_to
         mapping_keys = [y.split() for y in rest.strip().split('\n')]
         from_ranges = list(chain.from_iterable([list(range(int(x[1]),int(x[1])+int(x[2]))) for x in mapping_keys]))
         to_ranges = list(chain.from_iterable([list(range(int(x[0]),int(x[0])+int(x[2]))) for x in mapping_keys]))
-        print('x')
-        # maps += [Mapping(map_from,map_to,x[0],x[1],x[2]) for x in [y.split() for y in rest.strip().split('\n')]]
+        maps = {x:x for x in range(dict_range_start,dict_range_end+1)}
+        # for x in range(dict_range_start,dict_range_end+1):
+        #     maps[x]=x
+        maps.update({x: y for x,y in zip(from_ranges, to_ranges)})
+        almanac[map_to] = maps
+        dict_range_start = min(maps.values())
+        dict_range_end = max(maps.values())
 
-print(maps)
+def find_location(seed):
+    current_almanac = 'seed'
+    current_index = int(seed)
+    while current_almanac != 'location':
+        lookup_almanac = almanac_key[current_almanac]
+        current_index = almanac[lookup_almanac][current_index]
+        current_almanac = lookup_almanac
+    # print(f'Seed: {seed} - {lookup_almanac} : {current_index}')
+    return current_index
+
+
+for s in seed_list:
+    find_location(s)
+
+lowest_location = min([find_location(seed) for seed in seed_list])
+print(f'Lowest Location: {lowest_location}')
