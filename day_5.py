fa3a4a928482d87635b4dec18b8a300ab3ccac5e
ClawhammerLobotomy <dansleeman@gmail.com>
2023-12-05 22:42:34 -0500

    day 5

--- day_5.py
+++ day_5.py
@@ -1,22 +1,41 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+from itertools import chain
 INPUT_FILE = 'd5_input.txt'
 INPUT_FILE = 'd5_test_input.txt'
 
 with open(INPUT_FILE, 'r') as f:
     LINE_LIST = f.read()
 
-# class Mapping():
-#     def __init__(self):
-#         self.
-Mapping = namedtuple('Mapping',['map_type','source_start','destination_start','range_length'])
+"""
+Each mapping needs a dictionary of from:to pairs in the range
+Starting values = list(range(x[1],x[1]+x[2]))
+Seed={
+98:50, (50 98 2)
+99:51, 
+50:52, (52 50 48)
+...
+97:99
+}
+"""
+class Mapping():
+    def __init__(self,first,rest):
+        self.map_from, to, self.map_to = first.split()[0].split('-')
+        self.map_ranges = rest
+
+Mapping = namedtuple('Mapping',['map_from','map_to','source_start','destination_start','range_length'])
 parts = LINE_LIST.split('\n\n')
-maps = []
+maps = defaultdict(int)
+# maps = []
 for l in parts:
     first, rest = l.split(':')
     if 'seeds' in first:
         seed_list = rest.split()
     else:
-        map_type = first.split()[0]
-        maps += [Mapping(map_type,x[0],x[1],x[2]) for x in [y.split() for y in rest.strip().split('\n')]]
+        map_from, to, map_to = first.split()[0].split('-')
+        mapping_keys = [y.split() for y in rest.strip().split('\n')]
+        from_ranges = list(chain.from_iterable([list(range(int(x[1]),int(x[1])+int(x[2]))) for x in mapping_keys]))
+        to_ranges = list(chain.from_iterable([list(range(int(x[0]),int(x[0])+int(x[2]))) for x in mapping_keys]))
+        print('x')
+        # maps += [Mapping(map_from,map_to,x[0],x[1],x[2]) for x in [y.split() for y in rest.strip().split('\n')]]
 
 print(maps)
