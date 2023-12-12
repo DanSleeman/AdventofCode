771cca48899f3ce57db198c3377232d621149244
Dan Sleeman <sleemand@shapecorp.com>
2023-12-12 10:25:48 -0500

    Day 8

--- day_8.py
+++ day_8.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from ast import literal_eval as make_tuple
 import math
 INPUT_FILE = 'd8_input.txt'
 # INPUT_FILE = 'd8_test_input.txt'
@@ -11,13 +12,13 @@ map_codes = LINE_LIST[1].split('\n')
 for m in map_codes:
     node_parts = m.split('=')
     node = node_parts[0].strip()
-    node_direction = [(x[0].replace('(','').strip(),x[1].replace(')','').strip()) for x in [node_parts[1].split(',')]][0]
+    node_direction = [(x[0].strip(),x[1].strip()) for x in [node_parts[1][2:-1].split(',')]][0]
     map_nodes[node] = node_direction
-def human_traverse():
-    current_node = map_nodes['AAA']
-    next_node = ''
+def human_traverse(start_node, end_node):
     steps = 0
-    while next_node != 'ZZZ':
+    next_node = start_node
+    current_node = map_nodes[next_node]
+    while next_node != end_node:
         for d in instruction:
             next_node = current_node[int(d)]
             steps +=1
@@ -33,11 +34,12 @@ def ghost_traverse(start_node):
             next_node = current_node[int(d)]
             steps +=1 
             current_node = map_nodes[next_node]
-            yield(steps,next_node)
+            yield(steps, next_node)
 
 starting_nodes = [x for x in map_nodes.keys() if x[-1] == 'A']
+human_steps = human_traverse('AAA','ZZZ')
 most_steps = math.lcm(*[e[0] for (s,*m,e) in [ghost_traverse(x) for x in starting_nodes]])
-human_steps = human_traverse()
+
 
 print(f'Human steps taken: {human_steps}') # 16579
 print(f'Ghost steps taken: {most_steps}')
