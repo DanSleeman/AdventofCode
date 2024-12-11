8e4d9752d91d7059672479f6a22e57c915a20605
ClawhammerLobotomy <dansleeman@gmail.com>
2024-12-10 20:04:27 -0500

    Day 9 part 2 complete

--- d9.py
+++ d9.py
@@ -5,6 +5,7 @@ TEST = False
 TEST1 = 1928
 PART1 = 6291146824486
 TEST2 = 2858
+PART2 = 6307279963620
 if TEST:
     x = "2333133121414131402"
 
@@ -37,27 +38,26 @@ def defrag(file_space, empty_space):
     return defragged
 
 def defrag_2(file_ids, empty_dots):
-    # defragged = []
-    tried = []
+    file_count = len(file_ids)
     moved = []
-    # for i in file_ids:
-        # _id_ = file_ids[i]
-        # defragged.append(_id_)
-    for e in file_ids[::-1]:
-        if e in tried:
-            continue
-        tried.append(e)
+
+    for i,e in enumerate(file_ids[::-1]):
+        print(f'Trying to move file {e}')
+
         for j in range(len(empty_dots)):
+            # print(f'Looking for open spot in position {j}')
+            if j > file_count-i-1:
+                # print(f'{len(file_ids)-i}')
+                # print(f'Passed current position of file {e}. Stop search.')
+                break
             space_remaining = [x for x in empty_dots[j] if x == '.']
             if len(e) <= len(space_remaining):
-                # defragged.append(e)
                 idx = 0
                 for q, dot in enumerate(empty_dots[j]):
                     if idx >= len(e):
                         continue
                     if dot != '.':
                         continue
-                    # dot = e[idx]
                     empty_dots[j][q] = e[idx]
                     idx += 1
                 moved.append(e)
@@ -65,9 +65,9 @@ def defrag_2(file_ids, empty_dots):
     remaining_files = []
     for f in file_ids:
         if f in moved:
-            f = ['.' for n in f]
+            f = ['.' for _ in f]
         remaining_files.append(f)
-    defragged = list(chain.from_iterable(chain.from_iterable(list(zip(remaining_files,empty_dots)))))
+    defragged = list(chain.from_iterable(chain.from_iterable(zip(remaining_files,empty_dots))))
     return defragged
 
 
@@ -79,7 +79,11 @@ def calc_checksum(defragged_list):
 
 defrag_list = defrag(file_space, empty_space)
 checksum = sum(calc_checksum(defrag_list))
+if TEST:
+    assert checksum == TEST1
 print(f"Part 1: {checksum}")
 defrag_list = defrag_2(file_ids, empty_space_dots)
-checksum = sum(calc_checksum(defrag_list))
+checksum = sum(calc_checksum(defrag_list)) # 6307280105216 - too high, off by one in index to stop searching.
+if TEST:
+    assert checksum == TEST2
 print(f"Part 2: {checksum}")
